447cee35a2ff9842477465293f65905fc06784e3
RoopeVaarama <roope.vaarama@gmail.com>
2019-09-10 14:35:22 +0300

    commit

---
+++ src/main/kotlin/ChatConnector.kt
@@ -0,0 +1,9 @@
+import java.io.InputStream
+import java.io.PrintStream
+
+public class ChatConnector(inputStream: InputStream, out: PrintStream) {
+
+    fun run(){
+
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,3 +2,5 @@ fun main(args: Array<String>) {
     val chatConnector = ChatConnector(System.`in`, System.out)
     chatConnector.run()
 }
+
+
